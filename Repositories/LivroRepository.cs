548ecfd6a463270fb62f84b2c3adb8b75d2f9fda
Felipe Augusto da Silva <felipeaugustodasilva23@gmail.com>
2022-09-01 04:03:23 -0300

    Atualização

---
+++ Repositories/LivroRepository.cs
@@ -0,0 +1,20 @@
+﻿using ChapterFT3.Controllers;
+using ChapterFT3.Models;
+
+namespace ChapterFT3.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly SqlContext _context;
+
+        public LivroRepository(SqlContext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+    }
+}
